b42278d66d4ca3120d8615866b8e6933b0536de7
nikhiljoshi <nikhil.joshi92@gmail.com>
2018-11-07 06:34:25 -0800

    Add internet permission in manifest file

---
+++ app/src/main/java/com/jonikhilshi/allsync/utility/Utility.java
@@ -0,0 +1,68 @@
+package com.jonikhilshi.allsync.utility;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Practice: get a URL and slap it on the screen - network calls!!
+ */
+public class Utility {
+
+    final static String GITHUB_BASE_URL =
+            "https://api.github.com/search/repositories";
+
+    final static String PARAM_QUERY = "q";
+
+    /*
+     * The sort field. One of stars, forks, or updated.
+     * Default: results are sorted by best match if no field is specified.
+     */
+    final static String PARAM_SORT = "sort";
+    final static String sortBy = "stars";
+
+    //Create the URL
+    public static URL createHttpUrl(String githubSearchQuery) {
+
+        Uri builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy)
+                .build();
+
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    //Open connection and grab the json
+    public static String getResponseFromHttp(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+}

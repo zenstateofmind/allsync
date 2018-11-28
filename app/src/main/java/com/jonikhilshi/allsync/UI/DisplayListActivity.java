be76d5d7877f04a9922122af69ed4422cb1dae26
nikhiljoshi <nikhil.joshi92@gmail.com>
2018-11-27 21:20:51 -0800

    Add logout option

--- app/src/main/java/com/jonikhilshi/allsync/UI/DisplayListActivity.java
+++ app/src/main/java/com/jonikhilshi/allsync/UI/DisplayListActivity.java
@@ -1,13 +1,21 @@
 package com.jonikhilshi.allsync.UI;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.jonikhilshi.allsync.Network.MyTwitterApi;
 import com.jonikhilshi.allsync.Network.MyTwitterApiClient;
@@ -44,6 +52,47 @@ public class DisplayListActivity extends AppCompatActivity {
         twitterListsRView.setLayoutManager(new GridLayoutManager(this, 3));
 
         twitterApi.getListOfTwitterList(twitterListsAdapter, Utility.LIST_SCREEN_NAME);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.display_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemClicked = item.getItemId();
+        if (itemClicked == R.id.logout_menu_item) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(DisplayListActivity.this);
+            builder.setMessage(getString(R.string.verify_user_wants_to_logout))
+                    .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                                CookieManager.getInstance().removeAllCookies(null);
+                                CookieManager.getInstance().flush();
+
+                            } else {
+                                CookieSyncManager cookieSyncMngr = CookieSyncManager.createInstance(getApplicationContext());
+                                cookieSyncMngr.startSync();
+                                CookieManager cookieManager = CookieManager.getInstance();
+                                cookieManager.removeAllCookie();
+                                cookieManager.removeSessionCookie();
+                                cookieSyncMngr.stopSync();
+                                cookieSyncMngr.sync();
+                            }
+
+
+                            Intent intent = new Intent(getApplication(), LoginActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+            builder.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
--- app/src/main/java/com/jonikhilshi/allsync/UI/LoginActivity.java
+++ app/src/main/java/com/jonikhilshi/allsync/UI/LoginActivity.java
@@ -37,7 +37,6 @@ public class LoginActivity extends AppCompatActivity {
 
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-
         // Setting up twitter config
         TwitterConfig config = new TwitterConfig.Builder(this)
                 .logger(new DefaultLogger(Log.DEBUG))
@@ -72,5 +71,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 }
